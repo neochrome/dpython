0690ec1a095bdadcee92b36de7a4cc44177d7a16
neochrome <johan@stenqvist.net>
2016-03-10 11:49:40 +0100

    Added support to execute as root

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-useradd -u $UID -g $GID -s /bin/bash -d /src -p pythonista pythonista || true
 cd /src
-su -m pythonista -c "eval \"$*\""
-exit $?
+if [[ $UID -eq 0 ]]; then
+	su -m -c "eval \"$*\""
+	exit $?
+else
+	useradd -u $UID -g $GID -s /bin/bash -d /src -p pythonista pythonista || true
+	su -m pythonista -c "eval \"$*\""
+	exit $?
+fi
